0be6423ed3bc6bd179dfd4c5dd66e1fe6de5a79d
saharabanu <saharabanungn@gmail.com>
2023-11-17 09:17:56 +0600

    some added

--- src/components/shared/ui/Destination.jsx
+++ src/components/shared/ui/Destination.jsx
@@ -3,161 +3,156 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../../firebase/firebase.auth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
-
-
+// import {Swiper, SwiperSlide} from 'swiper/react';
+// import { FreeMode, Pagination } from 'swiper/modules';
 
 const Destination = () => {
+  const [user] = useAuthState(auth);
+  const [items, setItems] = useState([]);
 
-  const [ user] = useAuthState(auth);
-  const [items,setItems] = useState([])
+  // for fecthing data
 
+  // const [items,setItems] = useState([])
+  // useEffect(()=>{
+  //     fetch('/data.json')
+  //     .then(res=>res.json())
+  //     .then(data=>setItems(data))
+  // },[]);
 
-  // for fecthing data 
+  // for searching destination
 
-    // const [items,setItems] = useState([])
-    // useEffect(()=>{
-    //     fetch('/data.json')
-    //     .then(res=>res.json())
-    //     .then(data=>setItems(data))
-    // },[]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searching, setSearching] = useState(false);
+  const navigate = useNavigate();
 
-    // for searching destination 
+  // const handleSearch = () => {
 
-    const [searchTerm, setSearchTerm] = useState('');
-    const [searching, setSearching] = useState(false);
-    const navigate = useNavigate();
+  //   setSearching(true);
 
-    
-    // const handleSearch = () => {
-     
-    //   setSearching(true);
-  
-      
-    //   setTimeout(() => {
-       
-    //     const filteredItems = items.filter(item =>
-    //       item.location.toLowerCase().includes(searchTerm.toLowerCase())
-    //     );
-    //     setItems(filteredItems);
-  
-        
-    //     setSearching(false);
-    //   }, 1000); 
-    // };
-  
-    useEffect(() => {
-      // Simulate an asynchronous operation (you can replace it with your actual search logic)
-      const searchItems = async () => {
-        // Set searching to true to indicate that search is in progress
-        setSearching(true);
-  
-        // Fetch data (replace with your actual fetch logic)
-        const response = await fetch('/data.json');
-        const data = await response.json();
-  
-        // Filter items based on the search term
-        const filteredItems = data.filter(item =>
-          item.location.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-        setItems(filteredItems);
-  
-        // Set searching back to false once the search is complete
-        setSearching(false);
-      };
-  
-      // Call the searchItems function when the searchTerm changes
-      searchItems();
-    }, [searchTerm]);
+  //   setTimeout(() => {
 
+  //     const filteredItems = items.filter(item =>
+  //       item.location.toLowerCase().includes(searchTerm.toLowerCase())
+  //     );
+  //     setItems(filteredItems);
 
-    const handleBooking = (id) => {
-      
-      
-      if (!user) {
-        navigate('/login');
-        return;
-      }
-  
-      // Display your animation here (you can use a modal or a dropdown)
-      const userName = prompt("Enter your name:");
-      const userEmail = prompt("Enter your email:");
-      const userPhone = prompt("Enter your phone:");
-      const desiredDate = prompt("Enter desired date:");
-  
-      // Save booking details to local storage
-      const bookingData = {
-        userName,
-        userEmail,
-        userPhone,
-        desiredDate,
-      };
-      localStorage.setItem(`booking_${id}`, JSON.stringify(bookingData));
-  
-      
-      toast.success("Booking successful!");
-  
-      
-      navigate('/');
+  //     setSearching(false);
+  //   }, 1000);
+  // };
+
+  useEffect(() => {
+    // Simulate an asynchronous operation (you can replace it with your actual search logic)
+    const searchItems = async () => {
+      // Set searching to true to indicate that search is in progress
+      setSearching(true);
+
+      // Fetch data (replace with your actual fetch logic)
+      const response = await fetch("/data.json");
+      const data = await response.json();
+
+      // Filter items based on the search term
+      const filteredItems = data.filter((item) =>
+        item.location.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setItems(filteredItems);
+
+      // Set searching back to false once the search is complete
+      setSearching(false);
     };
+
+    // Call the searchItems function when the searchTerm changes
+    searchItems();
+  }, [searchTerm]);
+
+  const handleBooking = (id) => {
+    if (!user) {
+      navigate("/login");
+      return;
+    }
+
+    // Display your animation here (you can use a modal or a dropdown)
+    const userName = prompt("Enter your name:");
+    const userEmail = prompt("Enter your email:");
+    const userPhone = prompt("Enter your phone:");
+    const desiredDate = prompt("Enter desired date:");
+
+    // Save booking details to local storage
+    const bookingData = {
+      userName,
+      userEmail,
+      userPhone,
+      desiredDate,
+    };
+    localStorage.setItem(`booking_${id}`, JSON.stringify(bookingData));
+
+    toast.success("Booking successful!");
+
+    navigate("/");
+  };
+
   
-
-
-
 
   return (
     <>
       <div className="bg-blue-900 mb-10 py-14">
-      <div className="px-16  ">
-        <div>
-          <h2 className="font-bold text-white ">Destination</h2>
+        <div className="px-16  ">
+          <div>
+            <h2 className="font-bold text-white ">Destination</h2>
 
-{/*  input for searching  */}
+            {/*  input for searching  */}
 
+            <div className="flex mt-4">
+              <input
+                type="text"
+                placeholder="Search by location"
+                value={searchTerm}
+                onChange={(e) => {
+                  setSearchTerm(e.target.value);
+                }}
+                className="px-4 py-2 border rounded mr-2 mb-5"
+              />
+            </div>
 
-<div className="flex mt-4">
-            <input
-              type="text"
-              placeholder="Search by location"
-              value={searchTerm}
-              onChange={(e) => {
-                setSearchTerm(e.target.value);
-                 
-              }}
-              className="px-4 py-2 border rounded mr-2 mb-5"
-            />
+            {searching && <p className="mt-4 text-gray-500">Searching...</p>}
           </div>
 
-          {searching && <p className="mt-4 text-gray-500">Searching...</p>}
+          <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-2 gap-8" >
 
 
-        </div>
 
-        <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-3">
-          {items?.map((item) =>(<div key={item?.id} >
-            <div className=" rounded-lg shadow-lg">
-             <a href={`/destination/${item?.id}`}>
-             <img src={item?.img} className="h-36 w-full" alt="img" />
-             </a>
-              <div className="bg-white px-3 py-5">
-                <p className="text-sm text-gray-400">{item?.location}</p>
-                <h4 className="font-semibold text-yellow-500 py-2">{item?.title}</h4>
-                <hr />
-                <div className="flex justify-between items-center py-2 ">
-                  <p className="text-sm text-gray-400">Starting From</p>
-                  <p className="font-semibold text-yellow-500">${item?.price}</p>
+
+
+          {items?.map((item) => (
+              <div key={item?.id}>
+                <div className=" rounded-lg shadow-lg">
+                  <a href={`/destination/${item?.id}`}>
+                    <img src={item?.img} className="h-36 w-full" alt="img" />
+                  </a>
+                  <div className="bg-white px-3 py-5">
+                    <p className="text-sm text-gray-400">{item?.location}</p>
+                    <h4 className="font-semibold text-yellow-500 py-2">{item?.title}</h4>
+                    <hr />
+                    <div className="flex justify-between items-center py-2 ">
+                      <p className="text-sm text-gray-400">Starting From</p>
+                      <p className="font-semibold text-yellow-500">${item?.price}</p>
+                    </div>
+                    <button
+                      onClick={() => handleBooking(item?.id)}
+                      className="bg-blue-900 text-white px-2 py-1 rounded"
+                    >
+                      Book Now
+                    </button>
+                    
+                  </div>
                 </div>
-                <button
-                  onClick={() => handleBooking(item?.id)}
-                  className="bg-blue-900 text-white px-2 py-1 rounded"
-                >
-                  Book Now
-                </button>
               </div>
-            </div>
-          </div>))}
-          
+            ))}
+
+
+
+            
+          </div>
         </div>
-      </div>
       </div>
     </>
   );
